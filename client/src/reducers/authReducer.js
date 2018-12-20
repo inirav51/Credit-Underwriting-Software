8aa5799466147d49fe94629298c4f464ed00f036
inirav51 <40270335+inirav51@users.noreply.github.com>
2018-12-20 17:50:56 -0500

    Update authReducer.js

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,10 +1,18 @@
 //For any action called related to authentication will be reduced here
-import {FETCH_USER} from '../actions/types'
+import {FETCH_USER, FETCH_FINCIALS, FETCH_FINCIALS_DETAILS} from '../actions/types'
 
-export default function(state = null, action){
+const initialState= {
+    financials: [],
+    financialDetails: {}
+}
+export default function(state = initialState, action){
     switch(action.type){
         case FETCH_USER:
             return action.payload || false;
+        case FETCH_FINCIALS:
+            return Object.assign({}, state, {financials: action.payload})
+        case FETCH_FINCIALS_DETAILS:
+            return Object.assign({}, state, {financialDetails: action.payload})
         default:
             return state;
     }
